00cd7b8a0c6347dcbc398e1c705d307ded3fb846
Med Tiouti <jm_tiouti@esi.dz>
2023-05-07 21:37:28 +0100

    Trying Video Tag Instead of iFrame

--- js/cours.js
+++ js/cours.js
@@ -8,11 +8,13 @@ const video = params.get('video');
 
 const apiLink = `http://192.168.137.49:81/api/${region}/${univ}/${cours}`
 
-const iframeVideo = document.getElementById('video-frame');
+const iframeVideo = document.getElementById('video-source');
 iframeVideo.src = `${apiLink}/Cours.mp4`;
 
 const iframePdf = document.getElementById('pdf-iframe');
 const iDataPdf = document.getElementById('pdf-data');
+console.log(iframePdf);
+console.log(iDataPdf);
 iframePdf.src = `${apiLink}/sample.pdf`;
 iDataPdf.data = `${apiLink}/sample.pdf`;
 
@@ -24,7 +26,6 @@ function showVideo() {
     
     document.getElementById("video-icon").classList.add("active");
     document.getElementById("pdf-icon").classList.remove("active");
-    // document.querySelector("#subnav .selected-icon").style.marginLeft = "0";
     console.log(document.getElementById("video-container").style)
     document.getElementById("video-container").style.display = "block";
     document.getElementById("pdf-container").style.display = "none";
@@ -33,7 +34,6 @@ function showVideo() {
 function showPDF() {
     document.getElementById("pdf-icon").classList.add("active");
     document.getElementById("video-icon").classList.remove("active");
-    // document.querySelector("#subnav .selected-icon").style.marginLeft = "50%";
     document.getElementById("pdf-container").style.display = "block";
     document.getElementById("video-container").style.display = "none";
 }
